0048c204f8d648cafa4523337fa44565fcb2f715
ahnafy <yasin011@morris.umn.edu>
2018-07-06 22:42:42 -0500

    add admin ts file to add functionality

---
+++ client/src/app/admin/admin.component.ts
@@ -0,0 +1,15 @@
+mport {Component} from '@angular/core';
+import {AppService} from "../app.service";
+import {Router} from "@angular/router";
+import {Observable} from 'rxjs/Observable'
+
+@Component({
+    selector: 'app-admin-component',
+    templateUrl: 'admin.component.html',
+    styleUrls: ['./admin.component.css'],
+    providers: [AppService]
+})
+export class AdminComponent {
+    constructor(private router: Router) {
+    }
+}
