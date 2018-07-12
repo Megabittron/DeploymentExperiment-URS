0fb66373c4dc39edffe037f2a4effca5217d9408
CMenne <menne019@morris.umn.edu>
2018-07-12 16:31:11 -0500

    Lowercase Server

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#Cleanup old Server/build files
-rm -r ~/Server 2>/dev/null
+#Cleanup old server/build files
+rm -r ~/server 2>/dev/null
 ./gradlew clean
 
 #Rebuild the project and extract it to home
 ./gradlew build -x RunClientTests
-cp Server/build/distributions/Server.tar ~/Server.tar -v
-tar xvf ~/Server.tar -C ~
+cp server/build/distributions/server.tar ~/server.tar -v
+tar xvf ~/server.tar -C ~
 
 #Get rid of the tar, and move 3601_run.sh executable into home
-rm ~/Server.tar
+rm ~/server.tar
 cp .3601_run.sh ~/3601.sh
 chmod +x ~/3601.sh
