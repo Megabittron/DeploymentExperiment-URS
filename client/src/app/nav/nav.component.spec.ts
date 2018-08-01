9cbd4cefe3ce46a670c064e2adaa7b2090f97888
CMenne <menne019@morris.umn.edu>
2018-08-01 16:43:48 -0500

    Add creation test

--- client/src/app/nav/nav.component.spec.ts
+++ client/src/app/nav/nav.component.spec.ts
@@ -2,6 +2,10 @@ import {TestBed, ComponentFixture} from '@angular/core/testing';
 import {NavComponent} from './nav.component';
 import {CustomModule} from '../custom.module';
 import {MATERIAL_COMPATIBILITY_MODE} from '@angular/material';
+import {AppModule} from "../app.module";
+import {RouterTestingModule} from "@angular/router/testing";
+import {routes} from "../app.routes";
+
 
 describe('Nav', () => {
 
@@ -10,16 +14,21 @@ describe('Nav', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [CustomModule],
-            declarations: [NavComponent], // declare the test component
+            imports: [
+                CustomModule,
+                AppModule,
+                RouterTestingModule.withRoutes(routes)
+            ],
             providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}],
         });
 
         fixture = TestBed.createComponent(NavComponent);
 
-        component = fixture.componentInstance; // BannerComponent test instance
-
+        component = fixture.componentInstance;
 
     });
 
+    it('should create the app', () => {
+        expect(fixture).toBeTruthy();
+    });
 });
