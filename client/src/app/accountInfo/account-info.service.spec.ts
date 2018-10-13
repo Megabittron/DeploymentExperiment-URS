fdfde61fcabcc8ef30eb0a4ef8679e4e3c397bfd
CMenne <menne019@morris.umn.edu>
2018-10-13 12:42:42 -0500

    Add HttpClientTestingModule to test imports #14

--- client/src/app/accountInfo/account-info.service.spec.ts
+++ client/src/app/accountInfo/account-info.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { AccountInfoService } from './account-info.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('AccountInfoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
       providers: [AccountInfoService]
     });
   });
