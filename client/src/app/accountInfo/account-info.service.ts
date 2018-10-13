2b858c257946ff29cd9881c7974415d26af60ac2
CMenne <menne019@morris.umn.edu>
2018-10-13 11:24:12 -0500

    Add editShirtSize api #14

--- client/src/app/accountInfo/account-info.service.ts
+++ client/src/app/accountInfo/account-info.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {User} from "../user";
+import {environment} from "../../environments/environment";
 
 @Injectable()
 export class AccountInfoService {
+    readonly baseUrl: string = environment.API_URL + 'user';
+    private editUserUrl: string = this.baseUrl;
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
+
+    editShirtSize(newUser: User) {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            }),
+        };
+
+        return this.http.put(this.editUserUrl + "/" + newUser._id, newUser, httpOptions);
+    }
 
 }
