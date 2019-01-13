5828dc09714f9c519506c47284ab7568fda26862
CMenne <menne019@morris.umn.edu>
2019-01-13 16:45:16 -0600

    Set up admin page basic information

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,15 +1,14 @@
 import {Component} from '@angular/core';
-import {AppService} from "../app.service";
-import {Router} from "@angular/router";
 import {Observable} from 'rxjs/Observable';
 
 @Component({
     selector: 'app-admin-component',
     templateUrl: 'admin.component.html',
     styleUrls: ['./admin.component.scss'],
-    providers: [AppService]
+    providers: []
 })
 export class AdminComponent {
-    constructor(private router: Router) {
+    constructor() {
+
     }
 }
