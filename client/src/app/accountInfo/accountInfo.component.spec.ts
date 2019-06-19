c1e804a6e2172da7f20dd3c485338cd15d326001
michael-small <small203@morris.umn.edu>
2019-06-18 20:04:29 -0500

    Create userUnitTest branch
    I was stuck on how to get unit tests for users to work, so this branch
    is saving my progress as I move onto other things.

--- client/src/app/accountInfo/accountInfo.component.spec.ts
+++ client/src/app/accountInfo/accountInfo.component.spec.ts
@@ -1,22 +1,70 @@
-import {TestBed, ComponentFixture} from '@angular/core/testing';
+import {TestBed, ComponentFixture, async} from '@angular/core/testing';
 import {DebugElement} from '@angular/core';
 import {CustomModule} from '../custom.module';
 import {AccountInfoComponent} from "./accountInfo.component";
+import {BehaviorSubject} from "rxjs/Rx";
+import {User} from "../user";
+import {AuthenticationService} from "../authentication.service";
+import {AccountInfoService} from "./account-info.service";
 
 describe('AccountInfo', () => {
 
     let component: AccountInfoComponent;
     let fixture: ComponentFixture<AccountInfoComponent>;
 
+    let acctServiceStub;
+    let authServiceStub: {
+        user$: BehaviorSubject<User>;
+    };
+
+    const user: User = {
+        _id: "string",
+        SubjectID: "string",
+        FirstName: "string",
+        LastName: "string",
+        ShirtSize: "string",
+        Role: "string"
+    };
+
     beforeEach(() => {
+
+        authServiceStub = {
+            user$: BehaviorSubject<user> = {
+                _id: "string",
+                SubjectID: "string",
+                FirstName: "string",
+                LastName: "string",
+                ShirtSize: "string",
+                Role: "string"
+            }
+        };
+
         TestBed.configureTestingModule({
             imports: [CustomModule],
             declarations: [AccountInfoComponent], // declare the test component
+            // providers: []
+            providers: [
+                {provide: AccountInfoService, useValue: acctServiceStub},
+                {provide: AuthenticationService, useValue: authServiceStub}]
         });
 
         fixture = TestBed.createComponent(AccountInfoComponent);
 
         component = fixture.componentInstance; // BannerComponent test instance
+    });
+
+    beforeEach(async(() => {
+        TestBed.compileComponents().then(() => {
+            fixture = TestBed.createComponent(AccountInfoComponent);
+            component = fixture.componentInstance;
+            fixture.detectChanges();
+
+            component.user.ShirtSize = 'XL';
+        });
+    }));
+
+    it('getName() gets the name of the user', () => {
+        component.getShirtSize();
     });
 
 });
