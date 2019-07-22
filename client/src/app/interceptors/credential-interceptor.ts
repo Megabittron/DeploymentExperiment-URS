a8f2b1d0358e55350e0aa87572f792312138ae98
Charles Menne <menne019@morris.umn.edu>
2019-07-22 18:18:21 -0500

    Implement credential interceptor
    Adds the option {withCredentials: true} to every request. This is needed for server sessions to work.

--- client/src/app/interceptors/credential-interceptor.ts
+++ client/src/app/interceptors/credential-interceptor.ts
@@ -1,2 +1,20 @@
-export class CredentialInterceptor {
+import {Injectable} from "@angular/core";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+/**
+ * Add credentials to every request. Server sessions don't function without this
+ * as the JSESSIONID cookie won't be sent.
+ */
+@Injectable()
+export class CredentialInterceptor implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        req = req.clone({
+            withCredentials: true
+        });
+
+        return next.handle(req);
+    }
 }
