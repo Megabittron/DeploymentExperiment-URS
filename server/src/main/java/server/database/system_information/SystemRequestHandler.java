faa932eadc7d00228aec637972524d4ec2544a54
michael-small <small203@morris.umn.edu>
2019-07-30 16:31:29 -0500

    Add review-groups get & edit SystemRequestHandler.java methods
    Issue #100 (Implement drag and drop review group rearrangement)
    Epic #84 (Review Groups)

--- server/src/main/java/server/database/system_information/SystemRequestHandler.java
+++ server/src/main/java/server/database/system_information/SystemRequestHandler.java
@@ -1,9 +1,10 @@
 package server.database.system_information;
 
-import com.mongodb.BasicDBObject;
+import com.mongodb.BasicDBList;
 import com.mongodb.util.JSON;
 import spark.Request;
 import spark.Response;
+
 
 /**
  *
@@ -18,7 +19,7 @@ public class SystemRequestHandler {
     /**Method called from Server when the 'api/users' endpoint is received.
      * This handles the request received and the response
      * that will be sent back.
-     *@param req the HTTP request
+     * @param req the HTTP request
      * @param res the HTTP response
      * @return an array of users in JSON formatted String
      */
@@ -26,5 +27,21 @@ public class SystemRequestHandler {
     {
         res.type("application/json");
         return systemController.getAllSystemInformation();
+    }
+
+    public String getReviewGroups(Request req, Response res)
+    {
+        res.type("application/json");
+        return systemController.getReviewGroups();
+    }
+
+    public Boolean editReviewGroups(Request req, Response res)
+    {
+        res.type("application/json");
+
+        Object jsonObject = JSON.parse(req.body());
+        BasicDBList changedReviewGroups = (BasicDBList) jsonObject;
+
+        return systemController.editReviewGroups(changedReviewGroups);
     }
 }
