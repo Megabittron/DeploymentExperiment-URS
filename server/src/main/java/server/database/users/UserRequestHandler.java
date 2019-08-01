700aef206afd7635bfcbf4779d970748a0e5268e
Charles Menne <menne019@morris.umn.edu>
2019-08-01 17:02:58 -0500

    Refactor methods to use BasicDBObjects '.parse()'
    JSON from 'com.mongodb.util' is deprecated.

--- server/src/main/java/server/database/users/UserRequestHandler.java
+++ server/src/main/java/server/database/users/UserRequestHandler.java
@@ -1,99 +1,61 @@
 package server.database.users;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.util.JSON;
 import spark.Request;
 import spark.Response;
 
 public class UserRequestHandler {
 
     private final UserController userController;
-    public UserRequestHandler(UserController userController){
+
+    public UserRequestHandler(UserController userController) {
         this.userController = userController;
     }
 
-    /**Method called from Server when the 'api/users' endpoint is received.
+    /**
+     * Method called from Server when the 'api/users' endpoint is received.
      *
-     *@param req the HTTP request
+     * @param req the HTTP request
      * @param res the HTTP response
      * @return an array of users in a JSON formatted String
      */
-    public String getUsers(Request req, Response res)
-    {
+    public String getUsers(Request req, Response res) {
         res.type("application/json");
         return userController.getUsers(req.queryMap().toMap());
     }
 
-    public String editUsertShirtSize(Request req, Response res){
+    public String editUsertShirtSize(Request req, Response res) {
         res.type("application/json");
-        Object o = JSON.parse(req.body());
+        BasicDBObject dbO = BasicDBObject.parse(req.body());
 
         try {
-            if(o.getClass().equals(BasicDBObject.class))
-            {
-                try {
-                    BasicDBObject dbO = (BasicDBObject) o;
+            String userID = dbO.getString("_id");
+            String tShirtSize = dbO.getString("ShirtSize");
 
-                    String userID = dbO.getString("_id");
-                    String tShirtSize = dbO.getString("ShirtSize");
-
-                    return userController.editUsertShirtSize(userID, tShirtSize);
-                }
-                catch(NullPointerException e)
-                {
-                    return null;
-                }
-
-            }
-            else
-            {
-                System.err.println("Expected BasicDBObject, received " + o.getClass());
-                return null;
-            }
-        }
-        catch(RuntimeException ree)
-        {
-            ree.printStackTrace();
+            return userController.editUsertShirtSize(userID, tShirtSize);
+        } catch (Exception e) {
+            e.printStackTrace();
             return null;
         }
-
-
     }
 
-    public String editUserrole(Request req, Response res){
+
+    public String editUserrole(Request req, Response res) {
         res.type("application/json");
-        Object o = JSON.parse(req.body());
+        BasicDBObject dbO = BasicDBObject.parse(req.body());
 
         try {
-            if(o.getClass().equals(BasicDBObject.class))
-            {
-                try {
-                    BasicDBObject dbO = (BasicDBObject) o;
+            String userID = dbO.getString("userID");
+            String role = dbO.getString("position");
 
-                    String userID = dbO.getString("userID");
-                    String role = dbO.getString("position");
+            System.out.println("userID: " + userID);
+            System.out.println("position: " + role);
 
-                    System.out.println("userID: " + userID);
-                    System.out.println("position: " + role);
-
-                    return userController.editUserrole(userID, role);
-                }
-                catch(NullPointerException e)
-                {
-                    return null;
-                }
-
-            }
-            else
-            {
-                System.err.println("Expected BasicDBObject, received " + o.getClass());
-                return null;
-            }
-        }
-        catch(RuntimeException ree)
-        {
-            ree.printStackTrace();
+            return userController.editUserrole(userID, role);
+        } catch (Exception e) {
+            e.printStackTrace();
             return null;
         }
+
     }
 }
