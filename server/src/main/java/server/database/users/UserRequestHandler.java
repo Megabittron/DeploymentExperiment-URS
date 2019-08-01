82153dbb0c26965a7b8cdfead805e713e818f965
Charles Menne <menne019@morris.umn.edu>
2019-08-01 16:34:18 -0500

    Remove method getUserBySubjectID()
    This method isn't used. It's controller version is inside of LoginController.java. Getting a certain user can be done via the 'api/users' endpoint with the query param 'SubjectID' set to the requested users subjectID.

--- server/src/main/java/server/database/users/UserRequestHandler.java
+++ server/src/main/java/server/database/users/UserRequestHandler.java
@@ -12,32 +12,6 @@ public class UserRequestHandler {
         this.userController = userController;
     }
 
-    public String getUserBySubjectID(Request req, Response res){
-        res.type("application/json");
-        String id = req.params("subjectID");
-        String user;
-        try {
-            user = userController.getUserBySub(id);
-        } catch (IllegalArgumentException e) {
-            // This is thrown if the ID doesn't have the appropriate
-            // form for a Mongo Object ID.
-            // https://docs.mongodb.com/manual/reference/method/ObjectId/
-            res.status(400);
-            res.body("The requested user id " + id + " wasn't a legal Mongo Object ID.\n" +
-                "See 'https://docs.mongodb.com/manual/reference/method/ObjectId/' for more info.");
-            return "";
-        }
-        if (user != null) {
-            return user;
-        } else {
-            res.status(404);
-            res.body("The requested user with id " + id + " was not found");
-            return "";
-        }
-    }
-
-
-
     /**Method called from Server when the 'api/users' endpoint is received.
      *
      *@param req the HTTP request
