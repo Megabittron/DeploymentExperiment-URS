f234b8064445cf77fd0ced2083a87dbd4233456a
michael-small <small203@morris.umn.edu>
2019-08-02 14:54:18 -0500

    Navigate to individual submission from submission list
    Add basic html/scss/spec/ts/service for standalone submission pages.
    Pages right now are based on submissions' `_id`. The only thing on the
    card right now when navigated to is a barebone p tag.
    
    Issue #132 (Navigate to individual submission from submission list)
    Epic #131 (Submission Card View)

---
+++ client/src/app/submissionView/submissionView.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'submissionView.component',
+    templateUrl: 'submissionView.component.html',
+    styleUrls: ['./submissionView.component.scss'],
+    providers: []
+})
+
+export class SubmissionViewComponent {
+
+}
