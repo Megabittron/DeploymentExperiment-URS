1a9ed43ec913bc9d821d00d6b5c1f03de0b14fc3
Charles Menne <menne019@morris.umn.edu>
2019-08-12 17:05:07 -0500

    Change heredoc indentation scheme

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Test script"
 scp ${TRAVIS_BUILD_DIR}/server/build/distributions/server.tar ${DEPLOY_USER}@${MORRIS_HOST}:~
-ssh -tt ${DEPLOY_USER}@${MORRIS_HOST} <<EOF
+ssh -tt ${DEPLOY_USER}@${MORRIS_HOST} <<-EOF
     scp ~/server.tar ${DEPLOY_HOST}:~
     rm ~/server.tar
     ssh -tt ${DEPLOY_HOST}
