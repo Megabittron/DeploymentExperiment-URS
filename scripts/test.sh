3c640c2d0252c0cdd5e75c99e9ad2e66db93f7e8
Charles Menne <menne019@morris.umn.edu>
2019-08-12 15:16:05 -0500

    Add $ before env variable

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Test script"
 scp ${TRAVIS_BUILD_DIR}/server/build/distributions/server.tar ${DEPLOY_USER}@${MORRIS_HOST}:~
-ssh ${DEPLOY_USER}@{MORRIS_HOST}
+ssh ${DEPLOY_USER}@${MORRIS_HOST}
 scp ~/server.tar ${DEPLOY_HOST}:~
 rm ~/server.tar
 ssh ${DEPLOY_HOST}
