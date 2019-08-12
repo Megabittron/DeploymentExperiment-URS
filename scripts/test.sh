ddee2232dbe00213b484a9869d6ef9b3bbf1e281
Charles Menne <menne019@morris.umn.edu>
2019-08-12 14:42:38 -0500

    Test deploying server.tar to server

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 echo "Test script"
-scp -C ${TRAVIS_BUILD_DIR}/server/build/distributions/server.tar ${DEPLOY_USER}@${DEPLOY_HOST}:~
-
+scp ${TRAVIS_BUILD_DIR}/server/build/distributions/server.tar ${DEPLOY_USER}@${MORRIS_HOST}:~
+ssh ${DEPLOY_USER}@{MORRIS_HOST}
+scp ~/server.tar ${DEPLOY_HOST}:~
+rm ~/server.tar
+ssh ${DEPLOY_HOST}
+ls
