b58923d98cfbd16a6c010b46efd774913674b019
Charles Menne <menne019@morris.umn.edu>
2019-08-12 17:43:59 -0500

    Use indents instead of spaces

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+# This file needs to use indents and not spaces to work properly
 echo "Test script"
 scp ${TRAVIS_BUILD_DIR}/server/build/distributions/server.tar ${DEPLOY_USER}@${MORRIS_HOST}:~
 ssh -tt ${DEPLOY_USER}@${MORRIS_HOST} <<-EOF
-    scp ~/server.tar ${DEPLOY_HOST}:~
-    rm ~/server.tar
-    ssh -tt ${DEPLOY_HOST}
-    ls
-    tar -xvf ~/server.tar .
+	scp ~/server.tar ${DEPLOY_HOST}:~
+	rm ~/server.tar
+	ssh -tt ${DEPLOY_HOST} <<END
+		tar -xvf ~/server.tar
+		rm ~/server.tar
+		ls
+	exit
+	END
+	exit
 EOF
-
