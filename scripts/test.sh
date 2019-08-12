6d03ea88e448131fa7b278f56c1fc3af14a667d9
Charles Menne <menne019@morris.umn.edu>
2019-08-12 15:33:09 -0500

    Add here tag

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 echo "Test script"
 scp ${TRAVIS_BUILD_DIR}/server/build/distributions/server.tar ${DEPLOY_USER}@${MORRIS_HOST}:~
-ssh ${DEPLOY_USER}@${MORRIS_HOST}
-scp ~/server.tar ${DEPLOY_HOST}:~
-rm ~/server.tar
-ssh ${DEPLOY_HOST}
-ls
+ssh ${DEPLOY_USER}@${MORRIS_HOST} <<EOF
+    scp ~/server.tar ${DEPLOY_HOST}:~
+    rm ~/server.tar
+    ssh ${DEPLOY_HOST}
+    ls
+EOF
+
