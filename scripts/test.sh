d927ff18b4fa7e47b74985aa79fda04da6b94e40
Charles Menne <menne019@morris.umn.edu>
2019-08-12 16:31:08 -0500

    Remove inner heredoc

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,9 +4,8 @@ scp ${TRAVIS_BUILD_DIR}/server/build/distributions/server.tar ${DEPLOY_USER}@${M
 ssh -tt ${DEPLOY_USER}@${MORRIS_HOST} <<EOF
     scp ~/server.tar ${DEPLOY_HOST}:~
     rm ~/server.tar
-    ssh -tt ${DEPLOY_HOST} <<-END
-        ls
-        tar -xvf ~/server.tar .
-    END
+    ssh -tt ${DEPLOY_HOST}
+    ls
+    tar -xvf ~/server.tar .
 EOF
 
