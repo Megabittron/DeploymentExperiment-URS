ee951a17e2e7d79c2f74f3bff8662e2a8bbafbe3
Charles Menne <menne019@morris.umn.edu>
2019-08-15 13:42:08 -0500

    Add deployment commands

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 # This file needs to use indents and not spaces to work properly
 echo "Test script"
-scp ${TRAVIS_BUILD_DIR}/server/build/distributions/server.tar ${DEPLOY_USER}@${MORRIS_HOST}:~
-ssh -tt ${DEPLOY_USER}@${MORRIS_HOST} <<-EOF
-	scp ~/server.tar ${DEPLOY_HOST}:~
+scp ${TRAVIS_BUILD_DIR}/server/build/distributions/server.tar ${MORRIS_USER}@${MORRIS_HOST}:~
+ssh -tt ${MORRIS_USER}@${MORRIS_HOST} <<-EOF
+	scp ~/server.tar deploy-user@${DEPLOY_HOST}:~
 	rm ~/server.tar
-	ssh -tt ${DEPLOY_HOST} <<END
-		tar -xvf ~/server.tar
+	ssh -tt deploy-user@${DEPLOY_HOST} <<END
+		rm -r ~/server
+		tar -xf ~/server.tar
 		rm ~/server.tar
-		ls
-	exit
+		sudo rm -r /swadm/hosts/urs/www/*
+		sudo cp -r /home/deploy-user/server/lib/client/public/* /swadm/hosts/urs/www/
+		sudo systemctl restart urs.service &
+		exit
 	END
 	exit
 EOF
