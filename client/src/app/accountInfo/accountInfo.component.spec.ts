77b2570fbd172d7c17b085649104038ab21ac143
Charles Menne <menne019@morris.umn.edu>
2019-09-03 15:24:02 -0500

    Add test for component creation

--- client/src/app/accountInfo/accountInfo.component.spec.ts
+++ client/src/app/accountInfo/accountInfo.component.spec.ts
@@ -2,21 +2,66 @@ import {TestBed, ComponentFixture} from '@angular/core/testing';
 import {DebugElement} from '@angular/core';
 import {CustomModule} from '../custom.module';
 import {AccountInfoComponent} from "./accountInfo.component";
+import {AuthenticationService} from "../authentication.service";
+import {BehaviorSubject, Observable} from "rxjs";
+import {User} from "../user";
+import {AccountInfoService} from "./account-info.service";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('AccountInfo', () => {
 
     let component: AccountInfoComponent;
     let fixture: ComponentFixture<AccountInfoComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+
+    let authenticationServiceStub: {
+        isLoggedIn$: BehaviorSubject<boolean>;
+        isLoaded$: BehaviorSubject<boolean>;
+        user$: BehaviorSubject<User>;
+    };
+
+    let accountInfoServiceStub: {
+        saveShirtSize: (shirtSize: string, subjectId: string) => Observable<{ShirtSize: string}>
+    };
 
     beforeEach(() => {
+
+        authenticationServiceStub = {
+            isLoggedIn$: new BehaviorSubject(true),
+            isLoaded$: new BehaviorSubject(true),
+            user$: new BehaviorSubject({
+                _id: {
+                    $oid: "5b3524ba715fad363abdc3cc"
+                },
+                SubjectID: "99",
+                FirstName: "Bonita",
+                LastName: "Houston",
+                ShirtSize: "s",
+                Role: "user"
+            })
+        };
+
+        accountInfoServiceStub = {
+            saveShirtSize: (shirtSize, subjectId) => Observable.of({ShirtSize: shirtSize})
+        };
+
+
         TestBed.configureTestingModule({
-            imports: [CustomModule],
-            declarations: [AccountInfoComponent], // declare the test component
+            imports: [CustomModule, RouterTestingModule],
+            declarations: [AccountInfoComponent],
+            providers: [{provide: AuthenticationService, useValue: authenticationServiceStub},
+                        {provide: AccountInfoService, useValue: accountInfoServiceStub}]
         });
 
         fixture = TestBed.createComponent(AccountInfoComponent);
+        component = fixture.componentInstance;
+        de = fixture.debugElement;
+        el = de.nativeElement;
+    });
 
-        component = fixture.componentInstance; // BannerComponent test instance
+    it('should create AccountInfoComponent', function () {
+        expect(component).toBeDefined();
     });
 
 });
