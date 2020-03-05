d30104faccfe3f9a86ac45a949ea9023d90da309
michael-small <small203@morris.umn.edu>
2020-03-05 01:22:44 -0600

    Restructure home page by role, add role correcting page
    Role correcting page is a bit cheeky... mostly for my and Matt's
    amusement, and a stand in to remember to ask Matt for formal
    framing of the page.
    
    Issue #268 (handle student user permissions separate of admins)
    Epic #10 (Account Information)

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -15,11 +15,12 @@ export class HomeComponent {
     }
 
     public user: User;
-    date = new Date();
-    // date = new Date('04/26/2020 00:00'); //debug value
+    // date = new Date();
+    date = new Date('04/26/2020 00:00'); //debug value
 
     //TODO: This is going to be especially dated after this season.
     // The names are super verbose but I want it to be especially obvious that this is dated for 2020
+    weekOutFromAbstractsDue = new Date('03/20/2020 12:00');
     abstractsDueMarch28th2020atNoon = new Date('03/27/2020 12:00');
     reviewerFeedbackDueApril10th2020atTBD = new Date('04/10/2020 12:00');
     finalStudentRevisionsDueApril17th2020atTBD = new Date('04/17/2020 12:00');
